78cf6398fc48ebcd42b121a5a563f2f68dd800a0
Matt Wilson <william.wilson@uhhospitals.org>
2018-12-04 09:21:11 -0500

    WIP

--- samplepop.py
+++ samplepop.py
@@ -6,6 +6,7 @@ import random
 def set_up_args():
 
     ap = argparse.ArgumentParser()
+    ap.add_argument("pop_size", type=int)
     ap.add_argument("sample_size", type=int)
     return ap.parse_args()
 
@@ -14,24 +15,33 @@ def build_pop(pop_size):
     for i in range(pop_size):
 
         if random.random() < 0.55:
-            yield 'xx'
+            yield {"x":'xx', "i": i}
 
         else:
-            yield 'xy'
+            yield {"x":'xy', "i": i}
 
 
 if __name__ == "__main__":
 
     args = set_up_args()
 
-    print("sample size: {0} ({0!r})".format(args.sample_size))
+    pop = [x for x in build_pop(args.pop_size)]
 
-    xx_count = sum([1 for x in build_pop(args.sample_size) if x == "xx"])
+    pop_xx_count = sum(1 for x in pop if x["x"] == "xx")
 
     print("xx count for an {0}-element population is {1}, so {2:0.2f}%".format(
+        args.pop_size,
+        pop_xx_count,
+        pop_xx_count/float(args.pop_size)))
+
+    sample = random.sample(pop, args.sample_size)
+
+    sample_xx_count = sum(1 for x in sample if x["x"] == "xx")
+
+    print("xx count for an {0}-element sample is {1}, so {2:0.2f}%".format(
         args.sample_size,
-        xx_count,
-        xx_count/float(args.sample_size)))
+        sample_xx_count,
+        sample_xx_count/float(args.sample_size)))
 
 
 """
